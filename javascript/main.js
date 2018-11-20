2393090069435e79a6886b685f6e68136436b935
daniel <dmjames04@yahoo.com>
2018-11-20 15:40:38 +0000

    updates to responsiveness

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,7 @@
-main.js
+	let mainNav = document.getElementById('js-menu');
+	let navBarToggle = document.getElementById('js-navbar-toggle');
+
+	navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
