26b10a6f8c3c25430be55027cb6a2e10d3dd6166
daniel <dmjames04@yahoo.com>
2018-12-11 22:25:16 +0000

    updates

--- javascript/modal.js
+++ javascript/modal.js
@@ -3,7 +3,7 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
+var modalImg = document.getElementById("img");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
